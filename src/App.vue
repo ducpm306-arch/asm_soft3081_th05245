0e59d07a32ae7e53bdd6093348a1a75f0fed20e0
DELL <minhduc.59h3@gmail.com>
2026-05-28 09:22:53 +0700

    bruh

---
+++ src/App.vue
@@ -0,0 +1,163 @@
+<script setup></script>
+
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="d-flex align-items-center">
+          <img
+            src="https://media.istockphoto.com/id/1321617070/vector/health-medical-logo.jpg?s=612x612&w=0&k=20&c=sus8vhG3c__vCdvOBLDhuf2vPUgIAudIAeUBApU_7Ew="
+            alt="Logo"
+            class="d-inline-block align-text-top"
+            style="width: 50px; height: 50px"
+          />
+          <h6>BiTeeth</h6>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          <button type="button" class="btn btn-outline-primary">Dành cho Admin</button>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <section class="card text-bg d-flex align-items-center justify-content-center">
+    <img
+      src="https://img.magnific.com/free-vector/digital-health-medical-research-background-with-heartbeat-line_1017-50392.jpg?semt=ais_hybrid&w=740&q=80"
+      class="card-img"
+      alt="..."
+      style="height: 565px; object-fit: cover"
+    />
+    <div class="card-img-overlay" style="color: white">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">
+        This is a wider card with supporting text below as a natural lead-in to additional content.
+        This content is a little bit longer.
+      </p>
+    </div>
+  </section>
+  <br />
+  <section class="container">
+    <ul class="row col-12 justify-content-center">
+      <div class="card text-center col-3 mb-3">
+        <div class="card-body">
+          <h5 class="card-title">
+            <div
+              class="rounded-circle bg-primary bg-opacity-10 d-inline-flex align-items-center justify-content-center mx-auto mb-2"
+              style="width: 70px; height: 70px"
+            >
+              <img
+                src="https://cdn.phototourl.com/free/2026-05-26-8d86c9fa-f9db-418d-a70d-cd2a40268e5c.png"
+                alt="Icon"
+                style="width: 50px; height: 50px"
+              />
+            </div>
+          </h5>
+          <a href="#" class="btn btn-link">Tư vấn</a>
+        </div>
+      </div>
+      <div class="card text-center col-3 mb-3">
+        <div class="card-body">
+          <h5 class="card-title">
+            <div
+              class="rounded-circle bg-primary bg-opacity-10 d-inline-flex align-items-center justify-content-center mx-auto mb-2"
+              style="width: 70px; height: 70px"
+            >
+              <img
+                src="https://cdn.phototourl.com/free/2026-05-26-88c603f4-ddb1-4e00-ae58-048dddfd1306.png"
+                alt="Icon"
+                style="width: 50px; height: 50px"
+              />
+            </div>
+          </h5>
+          <a href="#" class="btn btn-link">Đặt dịch vụ</a>
+        </div>
+      </div>
+      <div class="card text-center col-3 mb-3">
+        <div class="card-body">
+          <h5 class="card-title">
+            <div
+              class="rounded-circle bg-primary bg-opacity-10 d-inline-flex align-items-center justify-content-center mx-auto mb-2"
+              style="width: 70px; height: 70px"
+            >
+              <img
+                src="https://cdn.phototourl.com/free/2026-05-26-f207f708-4524-4c7c-a221-04ebfd617689.png"
+                alt="Icon"
+                style="width: 50px; height: 50px"
+              />
+            </div>
+          </h5>
+          <a href="#" class="btn btn-link">Phòng khám</a>
+        </div>
+      </div>
+      <div class="card text-center col-3 mb-3">
+        <div class="card-body">
+          <h5 class="card-title">
+            <div
+              class="rounded-circle bg-primary bg-opacity-10 d-inline-flex align-items-center justify-content-center mx-auto mb-2"
+              style="width: 70px; height: 70px"
+            >
+              <img
+                src="https://cdn.phototourl.com/free/2026-05-26-e0a55d2c-b9a0-493d-a116-6a975fdb1424.png"
+                alt="Icon"
+                style="width: 50px; height: 50px"
+              />
+            </div>
+          </h5>
+          <a href="#" class="btn btn-link">Kiến thức</a>
+        </div>
+      </div>
+    </ul>
+  </section>
+  <br />
+  <section class="container">
+    <h1 style="text-align: center">Chăm sóc hàm răng Việt</h1>
+    <br />
+    <div class="row align-items-center justify-content-between">
+      <div class="card" style="width: 18rem">
+        <img
+          src="https://nhakhoaparkway.com/wp-content/uploads/2024/03/Nieng-rang-mac-cai_.jpg"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-title">Niềng răng</h5>
+          <a href="#" class="btn btn-outline-primary">Niềng răng</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem">
+        <img
+          src="https://nhakhoaparkway.com/wp-content/uploads/2024/01/Nho%CC%82%CC%89-ra%CC%86ng-kho%CC%82n.png"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <a href="#" class="btn btn-outline-primary">Nhổ răng khôn</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem">
+        <img
+          src="https://nhakhoaparkway.com/wp-content/uploads/2024/02/Di%CC%A3ch-vu%CC%A3-ha%CC%80n-tra%CC%81m-ra%CC%86ng-sa%CC%82u-ma%CC%A3%CC%86t-nhai.jpeg"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <a href="#" class="btn btn-outline-primary">Trám răng</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer></footer>
+</template>
+
+<style scoped></style>
